cdc84ed1ab2513370a2a0692fcdc425e28ac0fe4
Rayirth Dinesh <rayirthd@gmail.com>
2026-07-08 20:06:02 -0700

    demo: introduce bug/medium-1-importerror on main

--- src/aggregator.py
+++ src/aggregator.py
@@ -6,10 +6,10 @@ def _ensure_not_empty(transactions):
         raise ValueError("transactions list is empty")
 
 
-def total_value(transactions):
+def sum_value(transactions):
     """Return the sum of all transaction amounts."""
     _ensure_not_empty(transactions)
-    return sum(transactions[1:])
+    return sum(transactions)
 
 
 def average_value(transactions):
